c79274c14cc2649a3cea47e169500932d7502901
Pandey <Neetu.Pandey@FISGLOBAL.COM>
2020-07-01 20:17:55 +0530

    Counter example

--- apps/ngrx-store-app/src/app/app.module.ts
+++ apps/ngrx-store-app/src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { StoreModule } from '@ngrx/store';
@@ -13,14 +17,20 @@ import { CustomersModule } from '@fis/customers';
 import { CustomersViewComponent } from './customers-view/customers-view.component';
 import { CustomerAddComponent } from './customer-add/customer-add.component';
 
+import { counterReducer } from './counter/counter.reducer';
+import { MyCounterComponent } from './counter/my-counter/my-counter.component';
+
 @NgModule({
-  declarations: [AppComponent, CustomersViewComponent, CustomerAddComponent],
+  declarations: [AppComponent, CustomersViewComponent, CustomerAddComponent, MyCounterComponent],
   imports: [
     BrowserModule,
     CustomersModule,
+    MatButtonModule,
+    MatCardModule,
+    MatListModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
     StoreModule.forRoot(
-      {},
+      { count: counterReducer },
       {
         metaReducers: !environment.production ? [] : [],
         runtimeChecks: {
